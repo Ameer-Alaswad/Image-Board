bbbdc3a63d38cc5242991b46f2950382e3d7b4e1
Ameer Alaswad <ammeer.z.alaswad@gmail.com>
2021-03-01 08:22:03 +0100

    part 2 almost done

--- public/js/script.js
+++ public/js/script.js
@@ -1,22 +1,61 @@
-// const { AlexaForBusiness } = require('aws-sdk');
-
-console.log('klab');
 new Vue({
     el: '#main',
     data: {
-        name: 'fennel',
+        name: 'Irn Bru',
+        seen: true,
         images: [],
+        title: '',
+        description: '',
+        username: '',
+        file: null,
     },
     mounted: function () {
-        console.log('my main vue is ready');
-        // console.log('this.cities', this.cities);
+        // console.log('my main vue instance has mounted!');
+        // we will use axios to communicate with our server
+        // console.log('this.images: ', this.images);
+        // console.log('this: ', this);
         var self = this;
         axios
             .get('/images')
-            .then((response) => {
-                console.log('response', response.data);
+            .then(function (response) {
+                // console.log("this.images after axios: ", this.images);
+                // console.log('this after axios: ',this);
+                // console.log('response', response.data);
+                // console.log('self: ',self);
                 self.images = response.data;
             })
-            .catch((err) => console.log('err is get cities'));
+            .catch(function (err) {
+                console.log('error in axios', err);
+            });
+    },
+    methods: {
+        handleClick: function (e) {
+            // e.preventDefault()
+            var formData = new FormData();
+            formData.append('title', this.title);
+            formData.append('description', this.description);
+            formData.append('username', this.username);
+            formData.append('file', this.file);
+            console.log('this.title: ', this.title);
+            console.log('this.description: ', this.description);
+            console.log('this.username: ', this.username);
+            console.log('this.file: ', this.file);
+            axios
+                .post('/upload', formData)
+                .then(function (response) {
+                    console.log('response from post req: ', response);
+                })
+                .catch(function (err) {
+                    console.log('error from post req', err);
+                });
+        },
+        pete: function () {
+            console.log('hello');
+        },
+        handleChange: function (e) {
+            console.log('e.target.files[0]: ', e.target.files[0]);
+            console.log('handle change is running!');
+            this.file = e.target.files[0];
+        },
     },
 });
--- server.js
+++ server.js
@@ -1,9 +1,32 @@
 const express = require('express');
 const db = require('./db.js');
 const app = express();
+const multer = require('multer');
+const uidSafe = require('uid-safe');
+const path = require('path');
 
 app.use(express.static('public'));
+//////////////////////////
+//// configure multer
+const diskStorage = multer.diskStorage({
+    destination: function (req, file, callback) {
+        callback(null, __dirname + '/uploads');
+    },
+    filename: function (req, file, callback) {
+        uidSafe(24).then(function (uid) {
+            callback(null, uid + path.extname(file.originalname));
+        });
+    },
+});
 
+const uploader = multer({
+    storage: diskStorage,
+    limits: {
+        fileSize: 2097152,
+    },
+});
+///////////////////////////
+//////////////////
 app.get('/images', (req, res) => {
     db.getAllImages()
         .then(({ rows }) => {
@@ -11,4 +34,19 @@ app.get('/images', (req, res) => {
         })
         .catch((err) => console.log('err in get route server js', err));
 });
+//////////////////////////////
+////////
+app.post('/upload', uploader.single('file'), function (req, res) {
+    // If nothing went wrong the file is already in the uploads directory
+    if (req.file) {
+        res.json({
+            success: true,
+        });
+    } else {
+        res.json({
+            success: false,
+        });
+    }
+});
+
 app.listen(8080, () => console.log('image board running'));
